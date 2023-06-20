cdf376f16a7b60b7a22287737d0bbb13db8e145a
faithakinbobola <faithakinbobola2@gmail.com>
2023-06-20 11:41:24 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always Success (0)
+*/
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'z'; alp <= 'a'; alp--)
+	{
+		putchar(alp);
+
+	putchar('\n');
+	}
+	return (0);
+}

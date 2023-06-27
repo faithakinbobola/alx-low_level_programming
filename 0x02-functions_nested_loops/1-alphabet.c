13f6e988e82d635813a7120621a1973676248090
faithakinbobola <faithakinbobola2@gmail.com>
2023-06-27 13:35:14 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * A function that prints alphabets
+ *
+*/
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,9 @@
 
 #define HEADER_FILE
 
+#include "stdio.h"
+
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif

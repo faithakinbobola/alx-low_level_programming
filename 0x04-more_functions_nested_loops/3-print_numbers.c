c407364a04ddeca02122018f75335d84f1391789
faithakinbobola <faithakinbobola2@gmail.com>
2023-07-01 16:09:49 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - A function that prints the numbers
+ *
+ * Return: List of numbers
+*/
+
+void print_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}

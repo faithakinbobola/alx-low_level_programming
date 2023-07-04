cd575cff20d9765923ceef78c8eed272fdeeac8b
faithakinbobola <faithakinbobola2@gmail.com>
2023-07-04 08:01:37 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that updated a pointed value
+ *
+ * @n: A pointed parameter we set to 98
+ *
+ * Return: Returns nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

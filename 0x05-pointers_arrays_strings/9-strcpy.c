827bde6367def5029f81e2885cb83ec428ba6ec7
faithakinbobola <faithakinbobola2@gmail.com>
2023-07-04 13:10:50 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	char *copy = dest;
+	int a, b = 0;
 
-	while(*src)
-		*dest++ = *src++;
-	return (copy);
+	for (a = 0; src[a] != '\0'; ++a)
+	{
+		dest[b] = src[a];
+		++b;
+	}
+	dest[b] = '\0';
+
+	return (dest);
 }

ca5e3a262ecd9aa10d4f027a443574c74080c04e
faithakinbobola <faithakinbobola2@gmail.com>
2023-07-11 04:28:07 -0700

    update

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -3,9 +3,9 @@
  * main - a function that returns a string
  * Return: Always Success;
  */
+
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was
-	to be the best hacker\"\n\t- Kevin Mitnick");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t		- Kevin Mitnick");
 	return (0);
 }

090efbd0ad43325ba6fe02769a93a03d1914c5f1
faithakinbobola <faithakinbobola2@gmail.com>
2023-07-18 16:45:15 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a data type
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: A data type named dog with different properties
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
